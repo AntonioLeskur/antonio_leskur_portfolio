35641b5f1995c5d2519971ac7656848fcaf1c8d5
Antonio Leskur <antonioleskur93@gmail.com>
2024-10-03 20:44:20 +0200

    add download CV, model Contact, Footer

--- portfolio/pages/models.py
+++ portfolio/pages/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    your_email = models.EmailField()
+    title = models.TextField()
+    message = models.TextField()
+    slug = models.SlugField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
