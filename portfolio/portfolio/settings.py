8a5a051edfe61ee8c2df833714a05e8c1d714408
Antonio Leskur <antonioleskur93@gmail.com>
2024-10-14 21:37:34 +0200

    add bootstrap and imgs to forms

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'pages',
     'users',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
--- portfolio/users/views.py
+++ portfolio/users/views.py
@@ -1,16 +1,33 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
+from django import forms
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your views here.
+class CustomUserCreationForm(UserCreationForm):
+    username = forms.CharField(
+        label=_("Username"),
+        widget=forms.TextInput(attrs={'placeholder': 'Username'})
+    )
+    password1 = forms.CharField(
+        label=_("Password"),
+        widget=forms.PasswordInput(attrs={'placeholder': 'Password'})
+    )
+    password2 = forms.CharField(
+        label=_("Password"),
+        widget=forms.PasswordInput(attrs={'placeholder': 'Repeat your password'})
+    )
+
 def register_user(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             login(request, form.save())
             return redirect('/skif/')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'users/register.html', {'form': form})
 
 def login_user(request):
